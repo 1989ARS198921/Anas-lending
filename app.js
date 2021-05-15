a3112e6d0e00a33e8bf4bea16a9ccecec31acdff
Арсений Строилов <you@gmail.com>
2021-05-15 22:58:05 +0300

    enter

--- app.js
+++ app.js
@@ -2,11 +2,7 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 80
-
     app.use(express.static(__dirname + "/static"));
  
  
-app.listen(PORT, ()=>{
-    console.log('Server has been started ...')
-});
+app.listen(5000);
